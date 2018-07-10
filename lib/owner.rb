93dbc5ebd9b968576b9c3fb3766b73a0019371e9
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-10 23:28:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,9 +26,20 @@ class Owner
   def say_species
     "I am a #{@species}."
   end
-  
+
   def buy_fish(name)
     new_fish = Fish.new(name)
     @pets[:fishes] << new_fish
   end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
+  
 end
